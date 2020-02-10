04a258e074db02c8a570e75b7b825961f3af3968
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2020-02-10 14:39:12 +0100

    add c-support plugin to vim

--- install.sh
+++ install.sh
@@ -2,13 +2,13 @@
 
 function doIt() {
 	# Put Bash config files into place
-	cp "bash/bashrc" ~/.bashrc 
+	cp "bash/bashrc" $HOME/.bashrc 
 
-	# Add PWD to ~/.bash_profile as DOTFILES_DIR so we can use it to reference other files from this directory
-	echo -e "$(head -n 1 bash/bash_profile)\nDOTFILES_DIR=$PWD\n\n$(tail -n +2 bash/bash_profile)" > ~/.bash_profile
+	# Add PWD to $HOME/.bash_profile as DOTFILES_DIR so we can use it to reference other files from this directory
+	echo -e "$(head -n 1 bash/bash_profile)\nDOTFILES_DIR=$PWD\n\n$(tail -n +2 bash/bash_profile)" > $HOME/.bash_profile
 
 	# Source our new bash file
-	source ~/.bash_profile;
+	source $HOME/.bash_profile;
 
 	# Checkout submodule for Vundle
 	if [[ ! -e vim/bundle/Vundle.vim/README.md ]]; then
@@ -17,10 +17,10 @@ function doIt() {
 	fi;
 
 	# Copy vim directory (to manage vim plugins)
-	cp -r "$PWD/vim" ~/.vim
+	cp -r "$PWD/vim/." $HOME/.vim
 
 	# Copy git configuration
-	cp git/gitconfig ~/.gitconfig
+	cp git/gitconfig $HOME/.gitconfig
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
