80901db8976a644d36d5317d8e3aab853a7cf956
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2022-11-11 08:40:15 +0100

    update bin scripts

--- bin/update-vkpbouw-theme.sh
+++ bin/update-vkpbouw-theme.sh
@@ -1,12 +1,18 @@
-cp ~/Downloads/samatex.zip s1.dvk.co:~/samatex.zip
-ssh s1.dvk.co zip -r vkpbouw-themes-backup.zip /var/www/www.vkpbouw.com/wp-content/themes/
-ssh s1.dvk.co unzip samatex.zip -d /var/www/www.vkpbouw.com/wp-content/themes/
-ssh s1.dvk.co zip -r vkpbouw-plugins-backup.zip /var/www/www.vkpbouw.com/wp-content/plugins/
-ssh s1.dvk.co unzip /var/www/www.vkpbouw.com/wp-content/themes/samatex/plugins/enovathemes-addons.zip -d /var/www/www.vkpbouw.com/wp-content/plugins/
-ssh s1.dvk.co unzip /var/www/www.vkpbouw.com/wp-content/themes/samatex/plugins/envato-market.zip -d /var/www/www.vkpbouw.com/wp-content/plugins/  
-ssh s1.dvk.co unzip /var/www/www.vkpbouw.com/wp-content/themes/samatex/plugins/js_composer.zip -d /var/www/www.vkpbouw.com/wp-content/plugins/  
-ssh s1.dvk.co unzip /var/www/www.vkpbouw.com/wp-content/themes/samatex/plugins/revslider.zip -d /var/www/www.vkpbouw.com/wp-content/plugins/  
-ssh s1.dvk.co sudo rm -r /tmp/nginx-cache/
+#!/usr/bin/env bash
+
+set -e 
+
+scp ~/Downloads/samatex.zip s1.dvk.co:~/samatex.zip
+
+ssh -t s1.dvk.co "zip -r vkpbouw-themes-backup.zip /var/www/www.vkpbouw.com/wp-content/themes/; \
+unzip samatex.zip -d /var/www/www.vkpbouw.com/wp-content/themes/; \
+zip -r vkpbouw-plugins-backup.zip /var/www/www.vkpbouw.com/wp-content/plugins/; \
+unzip /var/www/www.vkpbouw.com/wp-content/themes/samatex/plugins/enovathemes-addons.zip -d /var/www/www.vkpbouw.com/wp-content/plugins/; \
+unzip /var/www/www.vkpbouw.com/wp-content/themes/samatex/plugins/envato-market.zip -d /var/www/www.vkpbouw.com/wp-content/plugins/; \
+unzip /var/www/www.vkpbouw.com/wp-content/themes/samatex/plugins/js_composer.zip -d /var/www/www.vkpbouw.com/wp-content/plugins/; \
+unzip /var/www/www.vkpbouw.com/wp-content/themes/samatex/plugins/revslider.zip -d /var/www/www.vkpbouw.com/wp-content/plugins/; \
+sudo rm -r /tmp/nginx-cache/;\
+"
 
 
  
