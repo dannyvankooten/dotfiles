280ceaf413dd2b50085c74dfc27045e423ae2f08
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2022-12-11 20:18:26 +0100

    add golang to path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,6 +12,7 @@ set DOTFILES_DIR (find $HOME -maxdepth 3 -type d -name "dotfiles" | sed 1q)
 set -U fish_user_paths $HOME/.cargo/bin $fish_user_paths
 set -U fish_user_paths $HOME/.local/bin $fish_user_paths
 set -U fish_user_paths $DOTFILES_DIR/bin $fish_user_paths 
+set -U fish_user_paths /usr/local/go/bin
 
 # source ~/.env file if it exists
 if test -f $HOME/.env
