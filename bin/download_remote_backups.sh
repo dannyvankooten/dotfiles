7e721a61dfd232497ed48750a5bca62c36b50d92
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2023-10-12 12:07:21 +0200

    clean up

--- bin/download_remote_backups.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-rsync --recursive --delete --ignore-existing --compress exodus.mc4wp.com:~/backups/ ~/backups/exodus.mc4wp.com
-rsync --recursive --delete --ignore-existing --compress exodus.boxzillaplugin.com:~/backups/ ~/backups/exodus.boxzillaplugin.com
-rsync --recursive --delete --ignore-existing --compress genesis.dvk.co:~/backups/ ~/backups/genesis.dvk.co
