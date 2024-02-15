f3636cf9eff930cd697ed673c29dfbbb291ef305
Danny van Kooten <hi@dvk.co>
2024-02-15 22:50:11 +0100

    switch to nvim entirely

--- install.sh
+++ install.sh
@@ -8,8 +8,12 @@ cp .gitconfig $HOME/.gitconfig
 
 # Copy other configuration files into ~/.config
 cp -r .ssh $HOME/.ssh
-#cp -r .config/* $HOME/.config/
+cp -r .config/* $HOME/.config/
 cp .editorconfig $HOME/.editorconfig
 
-# Install packages 
+# Install vim-plug (https://github.com/junegunn/vim-plug)
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# Install packages
 xargs sudo apt-get -y install < packages.txt
